14f0d37f404d6938b7f027768531a140e06eeec3
Batli Joselevitz <batlijoselevitz@Batlis-MacBook-Pro.local>
2018-02-13 20:01:45 -0600

    another activity

--- 04-Stu_ConditionalRender/Unsolved/src/components/Navpills.js
+++ 04-Stu_ConditionalRender/Unsolved/src/components/Navpills.js
@@ -1,19 +1,25 @@
 import React from "react";
 
+const Navpill = props =>
+  <li onClick={()=> props.handlePageChange(props.pageName)}
+
+
+
 const Navpills = props =>
   <ul className="nav nav-tabs">
-    <li onClick={() => props.handlePageChange("Home")}>
+    <li onClick={() => props.handlePageChange("Home")}className={props.currentPage === "Home" ? "active" : " " }>
       <a>Home</a>
     </li>
-    <li onClick={() => props.handlePageChange("About")}>
+    <li onClick={() => props.handlePageChange("About")}className={props.currentPage === "About" ? "active" : " " }>
       <a>About</a>
     </li>
-    <li onClick={() => props.handlePageChange("Blog")}>
+    <li onClick={() => props.handlePageChange("Blog")}className={props.currentPage === "Blog" ? "active" : " " }>
       <a>Blog</a>
     </li>
-    <li onClick={() => props.handlePageChange("Contact")}>
+    <li onClick={() => props.handlePageChange("Contact")}className={props.currentPage === "Contact" ? "active" : " " }>
       <a>Contact</a>
     </li>
   </ul>;
 
 export default Navpills;
+
