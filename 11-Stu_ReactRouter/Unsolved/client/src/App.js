4c664dc1a70b827f8e2ebe68739f2ba16285fc90
Batli Joselevitz <batlijoselevitz@Batlis-MacBook-Pro.local>
2018-02-15 21:14:07 -0600

    day 2 activities continued

--- 11-Stu_ReactRouter/Unsolved/client/src/App.js
+++ 11-Stu_ReactRouter/Unsolved/client/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+iport { BroswerRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
 const App = () =>
+<Router>
   <div>
     <Nav />
-    <Books />
+    <Switch>
+      <Route exact path="/" component={Books} />
+      <Route exxact path="/books" component = {Books} />
+      <Route exact path="/books/:id" component = {Detail} />
+      <Route component={NoMatch}/>
+  </Switch>
   </div>;
+  </Router>
 
 export default App;
